2167138f56b50990166825b234bd20781d188b69
gauravkakkar1233 <9a10gauravkakkar@gmail.com>
2026-05-10 22:07:35 +0530

    new changes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-if(process.env.NODE_ENV!="production"){
-require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
 }
-// console.log(`Hello ${process.env.HELLO}`)
 
 const express = require("express");
 const app = express();
@@ -10,58 +9,53 @@ const path = require("path");
 const ejsmate = require("ejs-mate");
 const methodoverride = require("method-override");
 const ExpressError = require("./utils/ExpressError.js");
-const session=require("express-session");
+const session = require("express-session");
 const MongoStore = require("connect-mongo");
-const flash=require("connect-flash");
-const passport=require("passport");
-
-
-
+const flash = require("connect-flash");
+const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const User=require("./models/user.js");
 
-// const { resolveAny } = require("dns");
+const User = require("./models/user.js");
 
-const db_url=process.env.ATLASDB_URL;
-console.log(db_url);
+// ---------------- DB CONNECTION ----------------
+const db_url = process.env.ATLASDB_URL;
 
-async function main() {
-    await mongoose.connect(db_url);
+if (!db_url) {
+    throw new Error("ATLASDB_URL is not defined in .env file");
 }
-main()
-    .then(() => {
-        console.log("connection sucssesfull");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-let port = 3000;
 
+mongoose.connect(db_url)
+    .then(() => console.log("MongoDB connected successfully"))
+    .catch((err) => console.log("DB ERROR:", err));
+
+// ---------------- APP CONFIG ----------------
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsmate);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodoverride("_method"));
-app.engine('ejs', ejsmate);
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-
+// ---------------- SESSION STORE ----------------
 const store = MongoStore.create({
     mongoUrl: db_url,
     touchAfter: 24 * 3600,
-    // crypto: {
-    //     secret: process.env.SECRET,
-    // },
+    crypto: {
+        secret: process.env.SECRET,
+    },
 });
 
-store.on("error", function (err) {
-    console.log("SESSION STORE ERROR", err);
+store.on("error", (err) => {
+    console.log("SESSION STORE ERROR:", err);
 });
+
+// ---------------- SESSION OPTIONS ----------------
 const sessionoptions = {
     store,
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false,
-
     cookie: {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
@@ -69,76 +63,54 @@ const sessionoptions = {
     },
 };
 
-
 app.use(session(sessionoptions));
 app.use(flash());
 
+// ---------------- PASSPORT ----------------
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
- 
 
+// ---------------- LOCALS MIDDLEWARE ----------------
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    res.locals.currUser = req.user;
+    next();
+});
+
+// ---------------- ROUTES ----------------
 const listingrouter = require("./routes/listing.js");
 const reviewrouter = require("./routes/review.js");
 const userrouter = require("./routes/users.js");
 
-app.use((req,res,next)=>{
-    res.locals.message=req.flash("success");
-    res.locals.error=req.flash("error");
-    res.locals.currUser=req.user;
-    // console.log(res.locals.message)
-    next();
-});
-
-
-app.get("/demouser",async(req,res)=>{
-    let fakeuser=new User({
-        email:"delta@",
-        username:"Delta"
-    });
-
-    let registeredUser = await User.register(fakeuser, "helloworld");
-
-    console.log(registeredUser);
-    res.send(registeredUser);
-});
-
-// HOME ROUTE
-// app.get("/", (req, res) => {
-//     res.redirect("/listings");
-// });
-
-// USER ROUTES
-app.use("/",userrouter);
-
-// REVIEWS ROUTE 
-app.use("/listings/:id/reviews", reviewrouter);
-// LISTING ROUTES
+app.use("/", userrouter);
 app.use("/listings", listingrouter);
-app.get("/favicon.ico", (req, res) => res.status(204));
-app.use((req, res, next) => {
-    console.log("404 Route:", req.originalUrl);
+app.use("/listings/:id/reviews", reviewrouter);
+
+// favicon fix
+app.get("/favicon.ico", (req, res) => res.status(204).end());
+
+// ---------------- 404 HANDLER ----------------
+app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
 });
-app.use((err, req, res, next) => {
 
+// ---------------- ERROR HANDLER ----------------
+app.use((err, req, res, next) => {
     console.log("ERROR:", err);
 
     let { statusCode = 500, message = "Something went wrong!" } = err;
 
-    if (res.headersSent) {
-        return next(err);
-    }
-
     res.status(statusCode).render("listings/error.ejs", { err });
 });
 
-
-
+// ---------------- SERVER ----------------
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`App is listening to ${port}`);
+    console.log(`App is listening on port ${port}`);
 });
--- init/index.js
+++ init/index.js
@@ -1,36 +1,51 @@
-if(process.env.NODE_ENV!="production"){
-  require('dotenv').config();
-  }
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: "../.env" });
+}
 
 const mongoose = require("mongoose");
 const initdata = require("./data.js");
 const Listing = require("../models/listing.js");
 
-
 const db_url = process.env.ATLASDB_URL;
 
 console.log("DB URL =", db_url);
 
+// ---------------- DATABASE CONNECTION ----------------
 async function main() {
   await mongoose.connect(db_url);
 }
 
+// ---------------- DATABASE SEED FUNCTION ----------------
 const initdb = async () => {
+
+  // delete old listings
   await Listing.deleteMany({});
 
+  // add owner to each listing
   const dataWithOwner = initdata.data.map((obj) => ({
     ...obj,
-    owner: "6995f09b1de46f91716740ee",
+    owner: new mongoose.Types.ObjectId("681f2b3c8f1d6e7a12345678"),
   }));
 
+  // insert data
   await Listing.insertMany(dataWithOwner);
-  console.log("data was saved");
+
+  console.log("Database seeded successfully");
 };
 
+// ---------------- EXECUTION ----------------
 main()
   .then(async () => {
+
     console.log("Connection successful");
+
     await initdb();
+
     await mongoose.connection.close();
+
+    console.log("Connection closed");
+
   })
-  .catch(console.log);
+  .catch((err) => {
+    console.log("ERROR:", err);
+  });
