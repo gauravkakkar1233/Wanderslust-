b5e1e5522589b6887ee0cdca3111703b2dd2c9cb
gauravkakkar1233 <9a10gauravkakkar@gmail.com>
2026-05-10 22:26:49 +0530

    updated version

--- app.js
+++ app.js
@@ -6,56 +6,71 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
-const ejsmate = require("ejs-mate");
-const methodoverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const methodOverride = require("method-override");
 const ExpressError = require("./utils/ExpressError.js");
+
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
+
 const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
+const LocalStrategy = require("passport-local");
 
 const User = require("./models/user.js");
 
-// ---------------- DB CONNECTION ----------------
+// ---------------- DATABASE CONNECTION ----------------
+
 const db_url = process.env.ATLASDB_URL;
 
 if (!db_url) {
-    throw new Error("ATLASDB_URL is not defined in .env file");
+    throw new Error("ATLASDB_URL is missing in environment variables");
 }
 
-mongoose.connect(db_url)
-    .then(() => console.log("MongoDB connected successfully"))
-    .catch((err) => console.log("DB ERROR:", err));
+async function main() {
+    await mongoose.connect(db_url);
+}
+
+main()
+    .then(() => {
+        console.log("MongoDB Connected Successfully");
+    })
+    .catch((err) => {
+        console.log("Database Error:", err);
+    });
 
 // ---------------- APP CONFIG ----------------
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.engine("ejs", ejsmate);
+
+app.engine("ejs", ejsMate);
 
 app.use(express.urlencoded({ extended: true }));
-app.use(methodoverride("_method"));
+app.use(methodOverride("_method"));
+
 app.use(express.static(path.join(__dirname, "public")));
 
-// ---------------- SESSION STORE ----------------
+// ---------------- SESSION CONFIG ----------------
+
 const store = MongoStore.create({
     mongoUrl: db_url,
-    touchAfter: 24 * 3600,
     crypto: {
         secret: process.env.SECRET,
     },
+    touchAfter: 24 * 3600,
 });
 
-store.on("error", (err) => {
-    console.log("SESSION STORE ERROR:", err);
+store.on("error", () => {
+    console.log("ERROR in Mongo SESSION STORE", err);
 });
 
-// ---------------- SESSION OPTIONS ----------------
-const sessionoptions = {
+const sessionOptions = {
     store,
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false,
+
     cookie: {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
@@ -63,54 +78,71 @@ const sessionoptions = {
     },
 };
 
-app.use(session(sessionoptions));
+app.use(session(sessionOptions));
 app.use(flash());
 
-// ---------------- PASSPORT ----------------
+// ---------------- PASSPORT CONFIG ----------------
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 passport.use(new LocalStrategy(User.authenticate()));
+
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// ---------------- LOCALS MIDDLEWARE ----------------
+// ---------------- GLOBAL LOCALS ----------------
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     res.locals.currUser = req.user;
+
     next();
 });
 
 // ---------------- ROUTES ----------------
-const listingrouter = require("./routes/listing.js");
-const reviewrouter = require("./routes/review.js");
-const userrouter = require("./routes/users.js");
 
-app.use("/", userrouter);
-app.use("/listings", listingrouter);
-app.use("/listings/:id/reviews", reviewrouter);
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/users.js");
 
-// favicon fix
-app.get("/favicon.ico", (req, res) => res.status(204).end());
+app.use("/", userRouter);
+
+app.use("/listings", listingRouter);
+
+app.use("/listings/:id/reviews", reviewRouter);
+
+// ---------------- FAVICON FIX ----------------
+
+app.get("/favicon.ico", (req, res) => {
+    res.status(204).end();
+});
 
 // ---------------- 404 HANDLER ----------------
-app.all("*", (req, res, next) => {
-    next(new ExpressError(404, "Page Not Found!"));
+// FIXED FOR EXPRESS 5
+
+app.use((req, res, next) => {
+    next(new ExpressError(404, "Page Not Found"));
 });
 
 // ---------------- ERROR HANDLER ----------------
+
 app.use((err, req, res, next) => {
-    console.log("ERROR:", err);
 
-    let { statusCode = 500, message = "Something went wrong!" } = err;
+    console.log(err);
 
-    res.status(statusCode).render("listings/error.ejs", { err });
+    let { statusCode = 500, message = "Something Went Wrong" } = err;
+
+    res.status(statusCode).render("listings/error.ejs", {
+        err,
+    });
 });
 
 // ---------------- SERVER ----------------
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`App is listening on port ${port}`);
+    console.log(`Server Listening on Port ${port}`);
 });
